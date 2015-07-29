edbbda261a54dfdb3a7f4b10fbf1f4638e64b470
Timothy Shiu <timoshiu@yahoo.com>
2015-07-28 21:49:53 -0700

    minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
-// Static server
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
